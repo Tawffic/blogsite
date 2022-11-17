51db1732fd463c3d858532932ea4aa1f6bc2f075
Tawffic <sanitawfic@gmail.com>
2022-11-17 16:44:00 +0000

    edited most files

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, Articleview, AddpostView, UpdatePostView,DeletePostView
+from .views import HomeView, Articleview, AddpostView, UpdatePostView,DeletePostView,AddCategoryView,CategoryView,CategoryListView
 urlpatterns = [
    # path('', views.home, name="home")
    path('', HomeView.as_view(), name="home"),
    path('article/<int:pk>', Articleview.as_view(), name="article-detail"),
    path('add_post/',AddpostView.as_view(), name='add_post'),
+   path('add_category/',AddCategoryView.as_view(), name='add_category'),
    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+   path('category/<str:cats>/',CategoryView, name='category'),
+   path('category-list/',CategoryListView, name='category-list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView,CreateView,UpdateView,Dele
 from .models import Post
 from django.urls import reverse_lazy
 
-from .models import Post
+from .models import Post,category
 from .forms import PostForm,EditForm
 # Create your views here.
 #def home(request):
@@ -14,10 +14,23 @@ class HomeView(ListView):
     template_name ='home.html'
     ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context  ['cat_menu'] = cat_menu
+        return context
+
+
 
 class Articleview(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = category.objects.all()
+        context = super(Articleview, self).get_context_data(*args, **kwargs)
+        context  ['cat_menu'] = cat_menu
+        return context
 
 class AddpostView(CreateView):
     model = Post
@@ -25,6 +38,12 @@ class AddpostView(CreateView):
     template_name = 'add_post.html'
     #fields = '__all__'
 
+
+class AddCategoryView(CreateView):
+    model = category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
@@ -35,3 +54,14 @@ class DeletePostView(DeleteView):
     model= Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+def CategoryView(request,cats):
+    category_posts= Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats':cats.title(), 'category_posts':category_posts})
+
+
+
+
+def CategoryListView(request):
+    cat_menu_list = category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
